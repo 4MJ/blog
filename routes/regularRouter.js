bd936a4801889bb382cab29a7813ee1d99c4807d
4MJ <mjycho59@gmail.com>
2020-07-13 11:44:24 +0100

    changed from normal mysql to sync-mysql and other edits

--- routes/regularRouter.js
+++ routes/regularRouter.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require("mysql");
+const MySql = require("sync-mysql");
 
 // with database Create connection
-const conn = mysql.createConnection({
+// const conn = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'jesus4Life',
+//   database: 'blog',
+//   insecureAuth : true
+// });
+var conn = new MySql({
   host: 'localhost',
   user: 'root',
   password: 'jesus4Life',
-  database: 'blog',
-  insecureAuth : true
+
 });
- 
+var result = conn.query('USE blog;');
 //connect to database
-conn.connect((err) =>{
-  if(err) throw err;
-  console.log('Mysql Connected...');
-});
+
 
 //getting home page
 router.get('/', (req, res)=>{
-   res.render('index');
+  let sql = "SELECT * FROM blogpost;";
+    let result = conn.query(sql)
+    res.render('index',{result:result});
 });
 
 router.get('/posts/new', (req, res) => {
@@ -29,57 +34,41 @@ router.get('/posts/new', (req, res) => {
 //To post a new Post
 router.post('/posts/store', (req, res) => {
     let data = {author_name: req.body.username, blog_title: req.body.title, blog_desc: req.body.description, content: req.body.content};
-    let sql = "INSERT INTO blogpost SET ?";
-    let query = conn.query(sql, data,(err, results) => {
-      if(err) throw err;
-      res.redirect('/');
-    });
+    let sql = `INSERT INTO blogpost(author_name,blog_title, blog_desc,content) VALUES ('${data.author_name}', '${data.blog_title}', '${data.blog_desc}', '${data.content}')`;
+    conn.query(sql)
+    res.redirect('/');
 });
 
-//to get post
-router.get('/', async (req, res) => {
-    let sql = "SELECT * FROM blogpost";
-    let query = conn.query(sql,(err, results)=>{
-        if (err) {
-            throw err;
-        }
-        res.render('index', {
-            results: results
-        });
-    });
-});
 
 // //displaying a single post
 router.get('/post/:id', async (req, res) => {
   //TODO Check the id factor.
-    let sql= "SELECT blogpost.author_name, blogpost.blog_title, blogpost.blog_desc, blogpost.content FROM blogpost"; 
-    //"WHERE id="+id.params.id;
-    let query = conn.query(sql, (err, results)=>{
-        if (err){
-            throw err;
-        }
-        res.render('post',{
-           results: results
-        });
+  
+    let sql= "SELECT * FROM blogpost WHERE id="+req.params.id;
+    let result = conn.query(sql)
+    //res.json(result)
+    res.render('post',{result:result[0]});
     });
-});
 
 //route for update data
-router.post('/update',(req, res) => {
-    let sql = "UPDATE blogpost SET author_name='"+req.body.name+"', blog_title='"+req.body.title+"', blog_desc='"+req.body.description+"', content='"+req.body.content+"' WHERE id="+req.body.id;
-    let query = conn.query(sql, (err, results) => {
-      if(err) throw err;
-      res.redirect('/');
-    });
+router.get('/update',(req, res) => {
+    res.render('update');
+    // let sql = "UPDATE blogpost SET author_name='"+req.body.name+"', blog_title='"+req.body.title+"', blog_desc='"+req.body.description+"', content='"+req.body.content+"' WHERE id="+req.body.id;
+    // let query = conn.query(sql, (err, results) => {
+    //   if(err) throw err;
+    //   res.redirect('/');
+    // });
   });
    
-  //route for delete data
-  router.post('/delete',(req, res) => {
-    let sql = "DELETE FROM blogpost WHERE id="+req.body.id+"";
-    let query = conn.query(sql, (err, results) => {
-      if(err) throw err;
-        res.redirect('/');
-    });
+  // route for delete data
+  router.post('/delete/:id',(req, res) => {
+    let sql = "DELETE FROM blogpost WHERE id="+req.params.id+"";
+    let result = conn.query(sql);
+    res.render('index',{result:result});
+    // let query = conn.query(sql, (err, results) => {
+    //   if(err) throw err;
+    //     res.redirect('/');
+    // });
   });
 
 
